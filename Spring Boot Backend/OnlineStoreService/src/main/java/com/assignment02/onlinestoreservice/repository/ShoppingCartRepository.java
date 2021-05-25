343f7899c43ea50547e317acd7bd6f92f12b3272
Mohamed Akeel <67151688+AkeelMNM@users.noreply.github.com>
2021-05-25 21:28:57 +0530

    Spring Boot

---
+++ Spring Boot Backend/OnlineStoreService/src/main/java/com/assignment02/onlinestoreservice/repository/ShoppingCartRepository.java
@@ -0,0 +1,25 @@
+package com.assignment02.onlinestoreservice.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.assignment02.onlinestoreservice.domain.ShoppingCart;
+
+/**
+ * @author : A.M Zumry
+ * Registration Number : IT19175126
+ */
+
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart,Long> {
+
+	//get
+	@Query("select u from ShoppingCart u where u.userID = ?1")
+	List<ShoppingCart> getShopping(String userId);
+	
+	//get product IDs by userId
+	@Query("select u from ShoppingCart u where u.userID = ?1")
+	List<ShoppingCart> getAllProductIds(String userId);
+	
+}
